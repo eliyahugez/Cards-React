f9af2828b0ade2740b0ca8177f70038fe1a0312e
eliyahugez <121052211+eliyahugez@users.noreply.github.com>
2023-07-17 22:29:16 +0300

    24 hours

--- src/users/hooks/useUsers.js
+++ src/users/hooks/useUsers.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router";
 import { useUser } from "../providers/UserProvider";
 import useAxios from "../../cards/hooks/useAxios";
 import { getUserApi, EditUser, login, signup, getUsersApi } from "../services/usersApiService";
-import { getUser, removeToken, setTokenInLocalStorage } from "../services/localStorageService";
+import { getFromLocalStorage, getUser, removeToken, setTokenInLocalStorage, writeToLocalStorage } from "../services/localStorageService";
 import ROUTES from "../../routes/routesModel";
 import normalizeUser from "../helpers/normalization/normalizeUser";
 import { useSnackbar } from "../../providers/SnackbarProvider";
@@ -27,18 +27,48 @@ const useUsers = () => {
             setUser(user);
         }, [setUser]
     );
-
+    
+    
     const handleLogin = useCallback(async (user) => {
-        try {
-            const token = await login(user); // token: kjh34kl5h3lk45h345.k3jh45k3j4h5.kjh345kuj3h45
-            setTokenInLocalStorage(token); // localStorage: {token: kjh34kl5h3lk45h345.k3jh45k3j4h5.kjh345kuj3h45}
-            setToken(token);
-            const userFromLocalStorage = getUser();
-            requestStatus(false, null, null, userFromLocalStorage);
-            navigate(ROUTES.CARDS);
-        } catch (error) {
-            requestStatus(false, error, null);
-        }
+     
+    const loginAttempts = getFromLocalStorage("loginAttempts") || 0;
+    const loginTime = getFromLocalStorage("lastTimeThatUserTry")
+    const hours24Milliseconds = 24 * 60 * 60 * 1000;
+    const currentTime = Date.now();
+    const timeDifference = currentTime - loginTime;
+    // const blockUntil =loginTime + hours24Milliseconds -currentTime;
+    // const dateObject = new Date(timeDifference);
+    // const hours = dateObject.getHours();
+    // const minutes = dateObject.getMinutes();
+    const hours = Math.floor(hours24Milliseconds / (60 * 60 * 1000));
+const minutes = Math.floor((hours24Milliseconds % (60 * 60 * 1000)) / (60 * 1000));
+
+// console.log(loginTime + hours24Milliseconds -currentTime);
+// console.log("login ->",loginTime);
+// console.log("current ->",currentTime);
+// console.log("Difference ->",timeDifference);
+    if (timeDifference >= hours24Milliseconds) {
+      localStorage.removeItem("lastTimeThatUserTry");
+      writeToLocalStorage("loginAttempts",0)
+    }
+    if(loginAttempts < 3){
+      try {
+          const token = await login(user); // token: kjh34kl5h3lk45h345.k3jh45k3j4h5.kjh345kuj3h45
+          setTokenInLocalStorage(token); // localStorage: {token: kjh34kl5h3lk45h345.k3jh45k3j4h5.kjh345kuj3h45}
+          setToken(token);
+          const userFromLocalStorage = getUser();
+          requestStatus(false, null, null, userFromLocalStorage);
+          navigate(ROUTES.CARDS);
+          writeToLocalStorage("loginAttempts",0)
+          localStorage.removeItem("lastTimeThatUserTry");
+      } catch (error) {
+      const updeteloginAttempts =  loginAttempts+1
+          requestStatus(false, error, null);
+          writeToLocalStorage("loginAttempts",updeteloginAttempts)
+          writeToLocalStorage("lastTimeThatUserTry", Date.now())
+        
+      }
+  } else snack(`You try to many times Please try again in ${hours||"24"}:${minutes||"00"} hours `,"warning")  // ${hours||"24"}:${minutes||"00"}
     }, [navigate, requestStatus]);
 
     const handleLogout = useCallback(() => {
