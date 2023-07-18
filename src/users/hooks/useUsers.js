e00da84c08f14f5d8f4123cd632bfa6b17a7c95b
eliyahugez <121052211+eliyahugez@users.noreply.github.com>
2023-07-18 17:21:11 +0300

    24 with remainingTime

--- src/users/hooks/useUsers.js
+++ src/users/hooks/useUsers.js
@@ -30,31 +30,23 @@ const useUsers = () => {
     
     
     const handleLogin = useCallback(async (user) => {
-     
     const loginAttempts = getFromLocalStorage("loginAttempts") || 0;
     const loginTime = getFromLocalStorage("lastTimeThatUserTry")
     const hours24Milliseconds = 24 * 60 * 60 * 1000;
     const currentTime = Date.now();
     const timeDifference = currentTime - loginTime;
-    // const blockUntil =loginTime + hours24Milliseconds -currentTime;
-    // const dateObject = new Date(timeDifference);
-    // const hours = dateObject.getHours();
-    // const minutes = dateObject.getMinutes();
-    const hours = Math.floor(hours24Milliseconds / (60 * 60 * 1000));
-const minutes = Math.floor((hours24Milliseconds % (60 * 60 * 1000)) / (60 * 1000));
+    const remainingTime = hours24Milliseconds - timeDifference;
+    const hours = Math.floor(remainingTime / (60 * 60 * 1000));
+    const minutes = Math.floor((remainingTime % (60 * 60 * 1000)) / (60 * 1000));
 
-// console.log(loginTime + hours24Milliseconds -currentTime);
-// console.log("login ->",loginTime);
-// console.log("current ->",currentTime);
-// console.log("Difference ->",timeDifference);
     if (timeDifference >= hours24Milliseconds) {
       localStorage.removeItem("lastTimeThatUserTry");
       writeToLocalStorage("loginAttempts",0)
     }
     if(loginAttempts < 3){
       try {
-          const token = await login(user); // token: kjh34kl5h3lk45h345.k3jh45k3j4h5.kjh345kuj3h45
-          setTokenInLocalStorage(token); // localStorage: {token: kjh34kl5h3lk45h345.k3jh45k3j4h5.kjh345kuj3h45}
+          const token = await login(user); 
+          setTokenInLocalStorage(token); 
           setToken(token);
           const userFromLocalStorage = getUser();
           requestStatus(false, null, null, userFromLocalStorage);
@@ -68,7 +60,7 @@ const minutes = Math.floor((hours24Milliseconds % (60 * 60 * 1000)) / (60 * 1000
           writeToLocalStorage("lastTimeThatUserTry", Date.now())
         
       }
-  } else snack(`You try to many times Please try again in ${hours||"24"}:${minutes||"00"} hours `,"warning")  // ${hours||"24"}:${minutes||"00"}
+  } else snack(`You try to many times Please try again in ${hours||"24"}:${minutes||"00"} hours `,"warning")  
     }, [navigate, requestStatus]);
 
     const handleLogout = useCallback(() => {
